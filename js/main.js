b4e872b6eaa26f8addccc773cea06ecc1cffddd1
agentnice <50643457+agentnice@users.noreply.github.com>
2020-01-08 21:29:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+// Slick Top slider settings
+
+$('.top-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: "<img src='../img/arrow_prev.png' class='prev' alt='prev'>",
+    nextArrow: "<img src='../img/arrow_next.png' class='next' alt='next'>",
+    adaptiveHeight: true
+  });
+
+
+// Timeline slider settings
+
+$('.timeline-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: "<img src='../img/arrow_prev.png' class='tl-prev' alt='prev'>",
+    nextArrow: "<img src='../img/arrow_next.png' class='tl-next' alt='next'>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+  // Tabs script
+
+  $(document).ready(function(){
+	
+    $('ul.tabs li').click(function(){
+      var tab_id = $(this).attr('data-tab');
+  
+      $('ul.tabs li').removeClass('current');
+      $('.tab-content').removeClass('current');
+  
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+  
+  });
+
